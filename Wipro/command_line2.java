78902da06d6f5a9ed320447093e57353c63d73bf
Santhiya Saravanan <157127542+Santhiyaprostar@users.noreply.github.com>
2025-07-18 14:21:42 +0530

    Add files via upload

---
+++ Wipro/command_line2.java
@@ -0,0 +1,12 @@
+package Wipro;
+import java.util.*;
+public class command_line2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str="Welcome "+args[0];
+		System.out.print(str);
+
+	}
+
+}
